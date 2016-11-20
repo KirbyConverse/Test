5fa501526994e438eff1eca3c400e3415a60076b
KirbyConverse <kirbyconverses@gmail.com>
2016-11-19 19:14:03 -0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"Questions": {
+  	"A": "A for loop is a definite loop",
+		"B": "A for loop is a infinite loop",
+		"C": "A for loop is a stupid loop",
+		"D": "A for loop is a waste of time"
+		}
+};
